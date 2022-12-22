687d28e7061684c872c6b694caa14d9386a6473c
Chiara Arrivas <ritarrivas96@gmail.com>
2022-12-22 16:50:32 +0100

    esercizio 8

---
+++ components-05/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {HelloWorld} from "./components-5";
+
+export class App extends React.Component {
+    render() {
+        return <div> 
+                    <HelloWorld /> 
+                    <p>What a beautiful day!</p>
+                </div>
+    }
+} 
