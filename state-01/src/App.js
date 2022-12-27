c2e94df402f9ff5d675e33630c5ef9a9708868a3
Chiara Arrivas <ritarrivas96@gmail.com>
2022-12-27 11:33:32 +0100

    state-01

---
+++ state-01/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Counter } from "./counter"
+
+export class App extends React.Component {
+    render() {
+        return <div> 
+                    <Counter/>
+                </div>
+    }
+} 
