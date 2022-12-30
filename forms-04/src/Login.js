32e6157bac7599a810e0d02d1302a79326aaeec7
Chiara Arrivas <ritarrivas96@gmail.com>
2022-12-30 15:33:55 +0100

    forms-04

---
+++ forms-04/src/Login.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+export class Login extends React.Component {
+        state = {
+            name: "",
+            password: "",
+            remember: false,
+        }
+
+handleInputChange = (event) => {
+    const name = event.target.name
+    const type = event.target.type
+
+    this.setState({
+        [name]: type === "checkbox" ? event.target.checked : event.target.value,
+        
+    })
+}
+
+resetState = () => {
+    this.setState({
+        name: "",
+        password: "",
+        remember: false,
+    })
+}
+
+onLogin = () => {
+    console.log(this.state)
+}
+
+render() {
+    return (
+        <div>
+            <div>
+                <button type="button" onClick={this.onLogin} disabled={!this.state.name || !this.state.password ? true : false}>Login</button>
+                <button type="button" onClick={this.resetState}>Reset</button>
+            </div>
+            <div>
+                <input name="name" placeholder="Name" value={this.state.name} onChange={this.handleInputChange}/>
+                <input name="password" type="password" placeholder="Password" value={this.state.password} onChange={this.handleInputChange}/>
+                <input name="remember" id="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange}/> 
+                <label htmlFor="remember">Remember</label>
+            </div>
+        </div>
+    )
+}
+}
