89a95e45c2308ed5080a91c158b650d0c6963d60
Chiara Arrivas <ritarrivas96@gmail.com>
2023-01-01 17:07:05 +0100

    Styling-components-01

---
+++ styling-components-01/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {HelloWorld} from "./components-5";
+
+export class App extends React.Component {
+    render() {
+        return <div className="welcome"> 
+                    <HelloWorld /> 
+                </div>
+    }
+} 
