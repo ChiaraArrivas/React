6e25fa32e6165d08dddffe2bce5af94e828e10d6
Chiara Arrivas <ritarrivas96@gmail.com>
2023-01-11 09:33:54 +0100

    esercizi

--- use-effect-02/src/ClickCounter.js
+++ use-effect-02/src/ClickCounter.js
@@ -1,34 +1,35 @@
 import React from "react";
 import { useEffect, useState } from "react"
 
-export function ClickCounter({initialValue = 0}) {
+export function ClickCounter({ initialValue= 0}) {
     const [counter, upDateCounter] = useState(initialValue)
 
     useEffect(() => {
-        console.log(`The counter is ready`)
+        console.log("The counter is mounted")
 
         return () => {
-            console.log(`The counter is dead`)
-        }
+            console.log("The counter is dead")
+            upDateCounter(initialValue)}
     }, [])
 
     useEffect(() => {
         console.log(`The counter is ${counter}`)
-
+        
         return () => {
             console.log(`The counter was ${counter}`)
+            
         }
     }, [counter])
 
     function handleCounterIncrement() {
-        upDateCounter((initialValue) => initialValue +1)
-        }
+        upDateCounter((initialValue) => initialValue + 1)
+    }
 
     return (
         <div>
             <h1>Count: {counter}</h1>
             <button onClick={handleCounterIncrement}>Add</button>
-    </div>
+        </div>
     )
 }
 
--- use-effect-03/src/GithubUser.js
+++ use-effect-03/src/GithubUser.js
@@ -2,24 +2,36 @@ import React from "react";
 import { useEffect, useState } from "react"
 
 export function GithubUser({ user }) {
-    const [data, setData] = useState()
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     async function fetchGithubUser(user) {
         try {
+            setLoading(true)
             const response = await fetch(`https://api.github.com/users/${user}`)
             const json = await response.json()
-
-            setData(json)
+            setLoading(false)
+            setData(json)            
         } catch (error) {
-            console.error(error)
+            console.log(error)
+            setError(error)
+            setData(null)
         }
     }
 
     useEffect(() => {
+        if (!user) {
+            return
+        }
         fetchGithubUser(user)
     }, [user])
 
-    return <div>{data && <h1>The name is {data.name} and the Id is {data.id}</h1>}
-    </div>
-
+    return (
+        <div>
+            {loading && <p>Loading..</p>}
+            {error && <p>{error}</p>}
+            {data && <h1>The name is {data.name} and the Id is {data.id}</h1>}
+        </div>
+    )
 }
