7f96a1dc78981cb337a986a6a145b2b625100f5a
Chiara Arrivas <ritarrivas96@gmail.com>
2023-01-15 18:57:59 +0100

    reactrouter-01

---
+++ reactrouter-01/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Welcome } from "./Welcome";
+
+export function App() {
+        return (
+                <Routes>
+                    <Route path="/" element={<Welcome name="Chiara"/>} />
+                </Routes>
+        )
+    }
+
+
