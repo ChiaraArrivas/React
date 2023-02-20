3cff91ac3836a097ac563b67ec0c2d75990a5805
Chiara Arrivas <ritarrivas96@gmail.com>
2023-02-20 11:42:49 +0100

    state-01 state-02

--- state-01/src/counter.js
+++ state-01/src/counter.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 export class Counter extends React.Component {
-    state = {                   //inizializzo lo state impostando il valore su un oggetto  
-        count: 0,
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      //inizializzo lo state impostando il valore su un oggetto
+      count: 0,
+    };
+    setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count + 1,
+        };
+      });
+    }, 1000);
+  }
 
-    constructor(props) {
-        super(props)
-            setInterval(() => {
-                this.setState((state) => {
-                    return {
-                        count: state.count + 1,
-                    }
-                })
-            }, 1000)
-        }
-    
-    render() {
-        return <h1>Count: {this.state.count}</h1>
-    }   
-}       
+  render() {
+    return <h1>Count: {this.state.count}</h1>;
+  }
+}
--- state-02/src/counter.js
+++ state-02/src/counter.js
@@ -1,27 +1,25 @@
 import React from "react";
 
 export class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.initialValue,
+    };
 
-    state = {                  
-        count: this.props.initialValue,
-    }
+    setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count + this.props.increment,
+        };
+      });
+    }, this.props.timeout);
+  }
 
-    constructor(props) {
-        super(props)
-
-            setInterval(() => {
-                this.setState((state) => {
-                    return {
-                        count: state.count + this.props.increment,
-                    }
-                })
-            }, this.props.timeout)
-        }
-    
-    render() {
-        return <h1>Count: {this.state.count}</h1>
-    }   
-}   
+  render() {
+    return <h1>Count: {this.state.count}</h1>;
+  }
+}
 
 /*Counter.defaultVal = {
     initialValue: 0,
